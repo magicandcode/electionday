048ddca97d94647801bb63d02946cf168a1f9f2c
magicandcode <magicandcode@gmail.com>
2020-07-31 10:32:46 +0200

    Improve and add test cases for menu
    * Test menu formatting when printed
    * Test adding and setting menu attributes

--- electionday/navigation.py
+++ electionday/navigation.py
@@ -1,9 +1,37 @@
 class Menu:
 
-    def __init__(self, options = None):
+    def __init__(self,
+                 options = None,
+                 selectors = None,
+                 selector_padding: int = 2,
+                 selector_punctuation: str = '.',
+                 left_padding: int = 0,
+    ):
         if options is None:
             options = []
-        self.options = options
+        self.options = [str(option) for option in options]
+        if selectors is None:
+            selectors = []
+        self.selectors = [str(selector) for selector in selectors]
+        self.selector_padding: int = abs(selector_padding) or 2
+        self.selector_punctuation: str = str(selector_punctuation)
+        self.left_padding = int(left_padding)
 
     def show(self):
-        pass
+        left_padding_string: str = ' ' * self.left_padding
+        if self.selectors:
+            print('\n'.join(
+                f"{left_padding_string}{selector}{self.selector_punctuation}"
+                f"{' '*(self.selector_padding or 1)}{option}"
+                for option, selector in zip(self.options, self.selectors)))
+        else:
+            print('\n'.join(f'{left_padding_string}{option}'
+                            for option in self.options))
+
+
+if __name__ == "__main__":
+    menu = Menu(
+        ['One', 'Two', 'Three'], [1, 2, 3, 4, 5, 6], selector_padding=0,
+        selector_punctuation='.', left_margin=5)
+    print(menu.options)
+    menu.show()
--- electionday/tests/test_navigation.py
+++ electionday/tests/test_navigation.py
@@ -1,3 +1,6 @@
+import io
+import sys
+
 import pytest
 
 import electionday.navigation as navigation
@@ -11,7 +14,7 @@ def test_menu_has_default_empty_options():
     assert navigation.Menu().options == []
 
 
-def test_menu_init_sets_options():
+def test_menu_constructor_sets_options():
     options = ['One', 'Two', 'Three']
     got = navigation.Menu(options=options).options
     expected = options
@@ -20,3 +23,79 @@ def test_menu_init_sets_options():
 
 def test_menu_has_show_method():
     assert navigation.Menu().show() is None
+
+
+def test_menu_can_print_options(capsys):
+    options = ['One', 'Two', 'Three']
+    menu = navigation.Menu(options=options)
+    menu.show()  # Print menu
+    got = capsys.readouterr().out.strip()
+    expected = all(option in got for option in options)
+    assert expected
+
+
+def test_menu_has_default_empty_selectors():
+    assert navigation.Menu().selectors == []
+
+
+def test_menu_constructor_sets_selectors():
+    selectors= ['1.', '2.', '3.']
+    got = navigation.Menu(selectors=selectors).selectors
+    expected = selectors
+    assert got == expected
+
+
+def test_menu_can_print_options_with_selectors(capsys):
+    options, selectors = ['One', 'Two', 'Three'], [1, 2, 3]
+    menu = navigation.Menu(options=options, selectors=selectors)
+    menu.show()  # Print menu
+    got = capsys.readouterr().out.strip()
+    assert all(option in got for option in options)
+
+
+@pytest.mark.parametrize('options, selectors, options_count', [
+    (['One', 'Two', 'Three'], [1, 2, 3, 4], 3),
+    (['One', 'Two', 'Three'], [1, 2, 3], 3),
+    (['One', 'Two', 'Three'], [1, 2], 2),
+    (['One', 'Two', 'Three'], [1], 1),
+    (['One', 'Two', 'Three'], [], 3),
+])
+def test_menu_can_print_as_many_options_as_there_are_selectors(
+        options, selectors, options_count, capsys):
+    menu = navigation.Menu(options=options, selectors=selectors)
+    menu.show()  # Print menu
+    got = len(capsys.readouterr().out.rstrip().split('\n'))
+    assert got == options_count
+
+
+def test_menu_constructor_can_set_selector_punctuation(capsys):
+    options, selectors = ['One', 'Two', 'Three'], [1, 2, 3]
+    punctuation = ':'
+    menu = navigation.Menu(
+        options=options, selectors=selectors, selector_punctuation=punctuation)
+    menu.show()  # Print menu
+    got = capsys.readouterr().out.count(punctuation)
+    expected = len(options)
+    assert got == expected
+
+
+def test_menu_constructor_can_set_selector_padding(capsys):
+    options, selectors = ['One', 'Two', 'Three'], [1, 2, 3]
+    padding = 5
+    menu = navigation.Menu(
+        options=options, selectors=selectors, selector_padding=padding)
+    menu.show()  # Print menu
+    got = capsys.readouterr().out
+    expected = got.count(' '*padding) == len(options)
+    assert expected
+
+
+def test_menu_constructor_can_set_left_padding(capsys):
+    options, selectors = ['One', 'Two', 'Three'], [1, 2, 3]
+    left_padding = 5
+    menu = navigation.Menu(
+        options=options, selectors=selectors, left_padding=left_padding)
+    menu.show()  # Print menu
+    got = capsys.readouterr().out.rstrip().split('\n')
+    expected = all(row.startswith(' '*menu.left_padding) for row in got)
+    assert expected
